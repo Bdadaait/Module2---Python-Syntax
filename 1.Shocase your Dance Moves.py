eeac0756f2b9e2ec0e4f30cce93e129ec39e6c54
Bdadaait <bdadaait@gmail.com>
2024-09-30 13:43:54 -0500

    first commit

---
+++ 1.Shocase your Dance Moves.py
@@ -0,0 +1,15 @@
+# Task1 code corrections
+
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Task2 Your Mode Today
+
+The_mood = input("sad")
+if The_mood == "happy":
+    print("That's great to hear!")
+elif The_mood == "sad":
+    Print("I hope your day gets better")
---
+++ 3.Arithmetic Operations in Daily Life.py
@@ -0,0 +1,15 @@
+# Task1: Grocery Store Math
+
+bread_price = 2 
+peanut_butter_price = 3
+jelly_price = 2 
+total_cost = bread_price + peanut_butter_price + jelly_price
+print(total_cost)
+
+# Task2: Bank Interest
+
+initial_amount = 10000 
+interest_rate = 0.07     # 7% = $0.07 interest rate
+total_amount = initial_amount * (1 + interest_rate)
+
+print(f"total amount after a year: ${total_amount}")
